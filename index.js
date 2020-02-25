00fa248a357abd778137a7058227e84f2752b072
AssanSav <assane.savadogo81@gmail.com>
2020-02-25 13:02:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// function map(sourceArray, funct) {
-//     let result = []
-//     for (const element of sourceArray) {
-//         result.push(funct(element))
-//     }
-//     return result
-// }
+function map(sourceArray, funct) {
+    let result = []
+    for (const element of sourceArray) {
+        result.push(funct(element))
+    }
+    return result
+}
 
-// function reduce(sourceArray, funct, staringPoint =0) {
-//     let total;
-//     if (staringPoint) {
-//         total = staringPoint
-//     } else {
-//         total = sourceArray[0]
-//         sourceArray.shift()
-//     }
-//     sourceArray.forEach(elem => {
-//         total = funct(elem, total)
-//     })
-//     return total
-// }
+function reduce(sourceArray, funct, staringPoint =0) {
+    let total;
+    if (staringPoint) {
+        total = staringPoint
+    } else {
+        total = sourceArray[0]
+        sourceArray.shift()
+    }
+    sourceArray.forEach(elem => {
+        total = funct(elem, total)
+    })
+    return total
+}
